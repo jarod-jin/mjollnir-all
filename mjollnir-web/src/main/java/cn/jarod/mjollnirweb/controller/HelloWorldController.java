1a27913b8af2d5aff6e1e42cdbfbd7697fcc5dd4
jarod.jin <kira277@163.com>
2018-11-18 23:21:23 +0800

    Initial commit

---
+++ mjollnir-web/src/main/java/cn/jarod/mjollnirweb/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package cn.jarod.mjollnirweb.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String test1() {
+        return "hello jarod";
+    }
+
+}
---
+++ mjollnir-web/src/test/java/cn/jarod/mjollnirweb/controller/MjollnirWebApplicationTest.java
@@ -0,0 +1,41 @@
+package cn.jarod.mjollnirweb.controller;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebAppConfiguration
+@SpringBootTest
+public class MjollnirWebApplicationTest {
+
+    private MockMvc mvc;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new HelloWorldController()).build();
+    }
+    @Test
+    public void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(content().string(equalTo("hello jarod")));
+    }
+}
