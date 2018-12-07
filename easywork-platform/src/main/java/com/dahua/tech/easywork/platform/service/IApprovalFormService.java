14a068a5ebc1d91003a66d3c9402753b58c6355f
jarod.jin <kira277@163.com>
2018-12-07 18:05:47 +0800

    no message

--- easywork-platform/src/main/java/com/dahua/tech/easywork/platform/service/IApprovalFormService.java
+++
@@ -1,9 +0,0 @@
-package com.dahua.tech.easywork.platform.service;
-
-import com.dahua.tech.easywork.api.dto.platform.ApprovalFormDTO;
-import com.dahua.tech.easywork.core.dto.ResultDTO;
-import com.dahua.tech.easywork.platform.entity.ApprovalForm;
-
-public interface IApprovalFormService {
-    ResultDTO submit(ApprovalFormDTO approvalForm);
-}
